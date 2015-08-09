6da5094c17dafa246b224fbc7f1ad02dec0d889c
Michael Collins <michael.collins@nakedcoders.biz>
2015-08-09 09:43:17 -0700

    Implement procmon.ProcessMonitor variable
    I created example_test.go to demonstrate how to write debug messages to
    Process Monitor.
    
    I updated procmon.go. I defined the ProcessMonitor variable that will be
    used to send debug messages to Process Monitor. I created the Writer
    interface type.

--- procmon.go
+++ procmon.go
@@ -24,12 +24,45 @@ SOFTWARE.
 
 package procmon
 
+import "io"
+
+// ProcessMonitor is used to write debug messages to the Process Monitor log.
+// Debug messages should be less than 2048 characters in order to display
+// correctly in the log.
+//
+// ProcessMonitor is an io.Writer type and can be used with any code that
+// supports writing to an io.Writer instance.
+var ProcessMonitor Writer
+
+func init() {
+	ProcessMonitor = &nullProcessMonitor{}
+}
+
+// Writer defines an interface for writing debug messages to the Process
+// Monitor log. Writer extends io.Writere and defines WriteString for
+// efficiently wrriting strings directly to the Process Monitor log.
+type Writer interface {
+	io.Writer
+
+	// Writes a string message to the Process Monitor log.
+	WriteString(s string) (n int, err error)
+}
+
+// Default null implementation of the Process Monitor writer. This
+// implementation is used on non-Windows platforms or on Windows hosts when
+// Process Monitor is not installed.
 type nullProcessMonitor struct{}
 
+// Null implementation of i.Writer.Write. This function will return the length
+// of the byte array, making the caller believe that the write was successful
+// and that no error occurred.
 func (pm *nullProcessMonitor) Write(p []byte) (n int, err error) {
 	return len(p), nil
 }
 
+// Null implementation of WriteString. This function will return the length of
+// the string, making the caller believe that the write was successful and that
+// no error occurred.
 func (pm *nullProcessMonitor) WriteString(s string) (n int, err error) {
 	return len(s), nil
 }
